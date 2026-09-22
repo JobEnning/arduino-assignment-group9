5ee9b24dd6dd93d176351909ab7633c36e114dad
Marijke <74551947+marijkevkl@users.noreply.github.com>
2026-09-22 08:55:19 +0200

    Add analysis code and test data

---
+++ analyse/plot.py
@@ -0,0 +1,39 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+time, temp, pres, humi = np.genfromtxt("testdata_bme.dat", delimiter=',').T
+
+plt.figure()
+plt.plot(time, temp, '.-')
+plt.xlim(0, time[-1]+time[1])
+plt.xlabel("Tijd (s)")
+plt.ylabel(r"Temperatuur (°C)")
+plt.show()
+
+plt.figure()
+plt.plot(time, pres/100, '.-')
+plt.xlim(0, time[-1]+time[1])
+plt.xlabel("Tijd (s)")
+plt.ylabel("Druk (hPa)")
+plt.show()
+
+plt.figure()
+plt.plot(time, humi, '.-')
+plt.xlim(0, time[-1]+time[1])
+plt.xlabel("Tijd (s)")
+plt.ylabel("Relatieve luchtvochtigheid (%)")
+plt.show()
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+time, co2 = np.genfromtxt("testdata_CO2.dat", delimiter=',').T
+
+plt.figure()
+plt.plot(time, co2, '.-')
+plt.xlim(0, time[-1]+time[1])
+plt.xlabel("Tijd (s)")
+plt.ylabel(r"CO$_2$-concentratie (ppm)")
+plt.show()
